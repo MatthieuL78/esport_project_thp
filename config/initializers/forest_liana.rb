4db709fd4c61bd01979c6593f5d43452ec7c3d51
MatthieuL78 <matthieu.lu@outlook.fr>
2018-06-05 15:38:24 +0200

    forest admin with data and dotenv for key

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,2 +1,8 @@
-ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
-ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+require 'dotenv-rails'
+
+ForestLiana.env_secret = ENV['FOREST_ADMIN_KEY']
+ForestLiana.auth_secret = ENV['FOREST_ADMIN_KEY']
+
+# ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
+# ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+# 88e36c9c149b80c23bf13becaaf18cc40a1456330a6963852ff38966e100ed3e
