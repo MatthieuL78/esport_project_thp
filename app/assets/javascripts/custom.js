bb477d3f4c24aec2de4f24c4dd8b5e06c1c5d647
geraldy1st <geraldy.leondas@gmail.com>
2018-06-05 16:38:30 +0200

    home page test

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+$(document).on ("turbolinks:load", function(){
+  $('#nav-toggle').click(function(e) {
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+    $('body').toggleClass('fixed');
+    $('#menu-app-slide').toggleClass('slider');
+    console.log("test");
+  });
+})
+
+alert("test");
