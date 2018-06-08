169fdb6c045352aebfead5eff12c0fc936f5cb0e
MatthieuL78 <matthieu.lu@outlook.fr>
2018-06-08 14:49:18 +0200

    concerns for model devise

---
+++ app/models/concerns/devise_concern.rb
@@ -0,0 +1,10 @@
+module DeviseConcern
+  extend ActiveSupport::Concern
+
+  included do
+  	# Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :trackable, :validatable
+    end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :games
   has_and_belongs_to_many :players
   has_and_belongs_to_many :sponsors
+  validates :name, :place, :gamen :date, presence: true
 end
