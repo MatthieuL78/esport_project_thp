6671a22e2444b2fb27873f007eacd7fc38750f56
MatthieuL78 <matthieu.lu@outlook.fr>
2018-06-10 22:54:24 +0200

    scrapping smash optimization

--- app/services/scrapping_smashgg.rb
+++ app/services/scrapping_smashgg.rb
@@ -30,43 +30,48 @@ def my_url(nb_of_event, page)
   url_first_part = 'https://smash.gg/tournaments?per_page='
   nb_per_page = nb_of_event
   url_second_part = '&filter='
-  game = 'street fighter'
+  game = 'Street fighter'
   my_page = '&page=' + page
   url_first_part + nb_per_page + url_second_part + game + my_page
 end
 
-# Create a Spreadsheet on google drive
-def save_data_on_spreadsheet(title, img, date, attend, place, game, style)
+def init_spreadsheet
   session = GoogleDrive::Session.from_config('config.json')
   ws = session.spreadsheet_by_key('161w9F2_0vwwRpfr4ggATvXL0J_xUW83-Q7Y5IffgyWY').worksheets[0]
-  ws[1, 1] = 'Title'
-  ws[1, 2] = 'Image'
-  ws[1, 3] = 'Date'
-  ws[1, 4] = 'Attend'
-  ws[1, 5] = 'Place'
-  ws[1, 6] = 'Game'
-  ws[1, 7] = 'Style'
+  title = ['Title', 'Image', 'Date', 'Attend', 'Place', 'Game', 'Style']
+  title.each_with_index { |title_value, index| ws[1, index + 1] = title_value }
+  ws
+end
 
-  (@my_data..@my_data + title.length).each_with_index do |row, index|
-    ws[row, 1] = title[index]
-    ws[row, 2] = img[index]
-    ws[row, 3] = date[index]
-    ws[row, 4] = attend[index]
-    ws[row, 5] = place[index]
-    ws[row, 6] = game[index]
-    ws[row, 7] = style[index]
+def save_excel(spreadsheet)
+  spreadsheet.save
+  spreadsheet.reload
+end
+
+# Create a Spreadsheet on google drive
+def data_to_excel(tournament_hash)
+  ws = init_spreadsheet
+  (@my_data..@my_data + tournament_hash['tr_title'].length).each_with_index do |row, index|
+    tournament_hash.size.times do |i|
+      ws[row, i + 1] = tournament_hash[tournament_hash.keys[i]][index]
+    end
   end
-  ws.save
-  ws.reload
-  @my_data += title.length
+  save_excel(ws)
+  @my_data += tournament_hash['tr_title'].length
 end
 
 # Scrap the infos
 def scrap(url, browser, game, style, nb_of_event)
-  tr_img = []
-  tr_style = []
-  tr_game = []
   title = ''
+  tournament = {
+    'tr_title' => [],
+    'tr_image' => [],
+    'tr_date' => [],
+    'tr_attend' => [],
+    'tr_place' => [],
+    'tr_game' => [],
+    'tr_style' => []
+  }
 
   browser.goto url
 
@@ -89,20 +94,20 @@ def scrap(url, browser, game, style, nb_of_event)
   attend_place = browser.divs(class: 'TournamentCardContainer')
 
   # Save in 3 differents tables
+  tournament['tr_title'] = title.map(&:text)
+
   img.each do |div|
     if div.image.exists?
-      tr_img << div.image.src
+      tournament['tr_image'] << div.image.src
     else
-      tr_img << ''
+      tournament['tr_image'] << ''
     end
   end
 
-  tr_title = title.map(&:text)
-
-  tr_date = date.map(&:text)
+  tournament['tr_date'] = date.map(&:text)
 
   # To improve
-  tr_attend = attend_place.map do |div|
+  tournament['tr_attend'] = attend_place.map do |div|
     my_result = div.divs(class: %w[InfoList__title InfoList__section]).map do |divbis|
       if divbis.a.exists?
         my_text = divbis.a.text.split('').take_while do |text|
@@ -119,7 +124,7 @@ def scrap(url, browser, game, style, nb_of_event)
   end
 
   # To improve
-  tr_place = attend_place.map do |div|
+  tournament['tr_place'] = attend_place.map do |div|
     my_result = div.divs(class: %w[InfoList__title InfoList__section]).map do
       if div.children[1].text[0].i? || div.children[1].text[0].nil?
         'ONLINE'
@@ -135,12 +140,12 @@ def scrap(url, browser, game, style, nb_of_event)
   end
 
   # Put this in an other function
-  title.length.times do
-    tr_game << game.capitalize
-    tr_style << style.capitalize
+  tournament['tr_title'].length.times do
+    tournament['tr_game'] << game.capitalize
+    tournament['tr_style'] << style.capitalize
   end
 
-  save_data_on_spreadsheet(tr_title, tr_img, tr_date, tr_attend, tr_place, tr_game, tr_style)
+  data_to_excel(tournament)
 end
 
 # Scraping data on smash GG for : Tournament
@@ -152,8 +157,8 @@ end
 
 def main
   # Add the following information :
-  my_game = 'street fighter'
-  style = 'combat'
+  my_game = 'Street fighter'
+  style = 'Combat'
   # End
 
   @my_data = 2
@@ -168,7 +173,9 @@ def main
     my_nb_event = '100'
     my_page += 1
     @nb_event_integer -= 100
-    url = my_url(my_nb_event, my_my_page.to_s)
-    scrap(url, browser, game, style, my_nb_event.to_i)
+    url = my_url(my_nb_event, my_page.to_s)
+    scrap(url, browser, my_game, style, my_nb_event.to_i)
   end
 end
+
+main
