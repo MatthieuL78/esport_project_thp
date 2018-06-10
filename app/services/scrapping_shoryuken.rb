5188a70863772efa3a7dbe3954f7f7238b85829e
MatthieuL78 <matthieu.lu@outlook.fr>
2018-06-10 19:24:08 +0200

    rubocop correction and hash version

--- app/services/scrapping_shoryuken.rb
+++ app/services/scrapping_shoryuken.rb
@@ -16,7 +16,7 @@ def my_url(country, game)
 end
 
 # Create a Spreadsheet on google drive
-def save_data_on_spreadsheet(index_country, index_inter, name, team, character, actual_score, tournament, country)
+def save_data_on_spreadsheet(player_hash)
   session = GoogleDrive::Session.from_config('config.json')
   ws = session.spreadsheet_by_key('161w9F2_0vwwRpfr4ggATvXL0J_xUW83-Q7Y5IffgyWY').worksheets[1]
   ws[1, 1] = 'Rank country'
@@ -28,15 +28,16 @@ def save_data_on_spreadsheet(index_country, index_inter, name, team, character, 
   ws[1, 7] = 'Number of tournament'
   ws[1, 8] = 'Country'
 
-  (2..index_country.length).each_with_index do |row, index|
-    ws[row, 1] = index_country[index]
-    ws[row, 2] = index_inter[index]
-    ws[row, 3] = name[index]
-    ws[row, 4] = team[index]
-    ws[row, 5] = character[index]
-    ws[row, 6] = actual_score[index]
-    ws[row, 7] = tournament[index]
-    ws[row, 8] = country[index]
+  (2..player_hash['tr_index_country'].length).each_with_index do |row, index|
+    # ws[row, 1] = player_hash['tr_index_country'][index]
+    # ws[row, 2] = player_hash['index_inter'][index]
+    # ws[row, 3] = player_hash['name'][index]
+    # ws[row, 4] = player_hash['team'][index]
+    # ws[row, 5] = player_hash['character'][index]
+    # ws[row, 6] = player_hash['actual_score'][index]
+    # ws[row, 7] = player_hash['tournament'][index]
+    # ws[row, 8] = player_hash['country'][index]
+    p index
   end
   ws.save
   ws.reload
@@ -44,14 +45,16 @@ end
 
 # Scrap the infos
 def scrap(url, browser, game, style)
-  tr_index_country = []
-  tr_index_inter = []
-  tr_name = []
-  tr_team = []
-  tr_character = []
-  tr_actual_score = []
-  tr_tournament = []
-  tr_country = []
+  player = { 
+    'tr_index_country' => [],
+    'tr_index_inter' => [],
+    'tr_name' => [],
+    'tr_team' => [],
+    'tr_character' => [],
+    'tr_actual_score' => [],
+    'tr_tournament' => [],
+    'tr_country' => []
+  }    
   content_to_scrapp = ''
 
   browser.goto url
@@ -61,33 +64,33 @@ def scrap(url, browser, game, style)
   content_to_scrapp = browser.tables(class: %w[table table-striped table-hover table-condensed])
   content_to_scrapp.each do |table|
     table.each_with_index do |row, index|
-      next if index == 0
+      next if index.zero?
       row.each_with_index do |col, index|
         case index
         when 0
-          tr_index_country << col.text
+          player['tr_index_country'] << col.text
         when 1
-          tr_index_inter << col.text
+          player['tr_index_inter'] << col.text
         when 2
-          tr_name << col.text
+          player['tr_name'] << col.text
         when 3
-          tr_team << col.text
+          player['tr_team'] << col.text
         when 4
-          tr_character << col.text
+          player['tr_character'] << col.text
         when 5
-          tr_actual_score << col.text
+          player['tr_actual_score'] << col.text
         when 6
-          tr_tournament << col.text
+          player['tr_tournament'] << col.text
         when 7
-          tr_country << col.text
+          player['tr_country'] << col.text
         else
           next
         end
       end
     end
   end
-
-  save_data_on_spreadsheet(tr_index_country, tr_index_inter, tr_name, tr_team, tr_character, tr_actual_score, tr_tournament, tr_country)
+  p player['tr_index_country'].length
+  # save_data_on_spreadsheet(player)
 end
 
 # Scraping data on smash GG for : Tournament
@@ -103,11 +106,11 @@ def main
   style = 'combat'
   my_country = 'france'
   # End
-  
+
   url = my_url(my_country, my_game)
   browser = Watir::Browser.new :firefox
   scrap(url, browser, my_game, style)
-  
+
 end
 
 main
