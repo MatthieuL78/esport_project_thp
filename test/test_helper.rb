2bf7ec3e405bbc4ac4d54b5624b2fcee33a68ef6
MatthieuL78 <matthieu.lu@outlook.fr>
2018-06-12 19:27:33 +0200

    chrome init test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+  chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+    )
+  end
 end
