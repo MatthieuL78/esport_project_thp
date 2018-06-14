11ab5e4122d1a01f8e9af8abdc924c464e2b8895
ZCLMK <zacharie.NTC@gmail.com>
2018-06-14 02:29:41 +0200

    forced commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_06_04_160116) do
-=======
 ActiveRecord::Schema.define(version: 2018_06_07_132232) do
->>>>>>> development
 
   create_table "events", force: :cascade do |t|
     t.string "name"
