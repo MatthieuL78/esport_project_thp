1de935171dad00a2e5eadd3f0c7d61a468c12fec
Geraldy <geraldy.leondas@gmail.com>
2018-06-16 22:35:14 +0200

    Update custom.js
    add graph

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -18,4 +18,74 @@ $(document).on ("turbolinks:load", function(){
   //   console.log(e)
   // });
 
+
+	//  Calcul de la valeur sur 100 pour le graphique
+	function percentageMe(valeur, plafond){
+		result = (valeur * 100) / plafond
+		return result
+	}
+// Récupération des stats du joueur (cachées dans la page pr l'instant)
+
+// -----------------------------------------------------------------------------------------------
+	var chartOptions = {
+		scale: {
+		  gridLines: {
+			 color: "black",
+			 lineWidth: .2
+		  },
+		  angleLines: {
+			 display: true
+		  },
+		  ticks: {
+			 beginAtZero: true,
+			 min: 0,
+			 max: 100,
+			 stepSize: 10
+		  },
+		  pointLabels: {
+			 fontSize: 18,
+			 fontColor: "black"
+		  }
+		},
+		legend: {
+		  position: 'top'
+		}
+	 };
+
+
+
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myRadarChart = new Chart(ctx, {
+		 // The type of chart we want to create
+		 type: 'radar',
+
+
+
+
+		 // The data for our dataset
+		 data: {
+			  labels: ["Classement", "Gain", "activité", "Streaming", "R.sociaux", "Exp", "Ratio W/L"],
+			  datasets: [{
+					label: "My First dataset",
+					backgroundColor: 'rgba(206, 251, 95,.8)',
+					borderColor: 'rgb(15, 13, 51)',
+					data: [
+						percentageMe(4000, 4000),
+						percentageMe(49000,50000),
+						percentageMe(3500, 4000),
+						percentageMe(20000,50000),
+						percentageMe(3000, 4000),
+						percentageMe(35000,50000),
+						percentageMe(4000, 4000)
+					],
+			  }]
+		 },
+
+
+		 // Configuration options go here
+
+		 options: chartOptions
+})
+
+
 })
